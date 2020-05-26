551911a61ed024e85d5d4f0f7069c58ae41efbe0
leandrorjbcn <36479789+leandrorjbcn@users.noreply.github.com>
2020-05-26 13:54:11 +0200

    Update Hello.java
    Adding comments in Hello.java

--- helloWorld/Hello.java
+++ helloWorld/Hello.java
@@ -1,3 +1,13 @@
+/**
+ * This class provides a implementation of the simple print to a String.
+ * 
+ * @author: Yandi Sarden
+ * @author: Leandro Rjbcn
+ * @version: 1.0, 26/05/2020
+ * @since: 1.0
+ * @see: String
+ */
+
 package helloWorld;
 
 public class Hello {
