4af30aea68c88ff75f6b1247296ed658f9526b8a
leandrorjbcn <36479789+leandrorjbcn@users.noreply.github.com>
2020-06-01 21:10:56 +0200

    Updates

---
+++ ngJJJavaSpringMySQLCRUD/src/main/java/com/example/ngJavaSpringMySQLCRUD/NgJavaSpringMySqlcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngJavaSpringMySQLCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgJavaSpringMySqlcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgJavaSpringMySqlcrudApplication.class, args);
+	}
+
+}
---
+++ ngJJJavaSpringMySQLCRUD/src/test/java/com/example/ngJavaSpringMySQLCRUD/NgJavaSpringMySqlcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngJavaSpringMySQLCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgJavaSpringMySqlcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
