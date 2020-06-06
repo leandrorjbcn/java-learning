afcd8aa331dff515efa2df71220b0f3fcd472475
leandrorjbcn <36479789+leandrorjbcn@users.noreply.github.com>
2020-06-06 19:58:07 +0200

    Update Power.java

--- p0005Power/src/p0005Power/Power.java
+++ p0005Power/src/p0005Power/Power.java
@@ -2,4 +2,15 @@ package p0005Power;
 
 public class Power {
 
+	/**
+	 * Receive two integers and return the division between them.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return an integer result of the division from a and b
+	 */
+	public static int division(int a, int b) {
+
+		return (int) Math.pow(a, b);
+	}
 }
