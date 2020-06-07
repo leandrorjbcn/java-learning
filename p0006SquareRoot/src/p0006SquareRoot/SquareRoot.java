b0754a8a977d2c2b60bde871200c9281115a0d6d
leandrorjbcn <36479789+leandrorjbcn@users.noreply.github.com>
2020-06-07 19:15:01 +0200

    Update SquareRoot.java

--- p0006SquareRoot/src/p0006SquareRoot/SquareRoot.java
+++ p0006SquareRoot/src/p0006SquareRoot/SquareRoot.java
@@ -1,5 +1,24 @@
+/**
+ * This class provides an implementation of the simple square root operation.
+ * 
+ * @author Leandro Rjbcn
+ * @version 1.0, 07/06/2020
+ * @since: 1.0
+ */
+
 package p0006SquareRoot;
 
 public class SquareRoot {
 
+	/**
+	 * Receive one integers and return the square root.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return an integer result of the multiplicaction from a and b
+	 */
+	public static int multiplication(int a, int b) {
+
+		return (a * b);
+	}
 }
