67892b698bbcb010138614d37772503ec54462f4
leandrorjbcn <36479789+leandrorjbcn@users.noreply.github.com>
2020-06-07 19:35:45 +0200

    Update Main.java

--- p0006SquareRoot/src/p0006SquareRoot/Main.java
+++ p0006SquareRoot/src/p0006SquareRoot/Main.java
@@ -1,9 +1,28 @@
 package p0006SquareRoot;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Variables
+		SquareRoot sqtRoot = null;
+		Scanner scanner = new Scanner(System.in);
+		int radicand = 0;
+		int root = 0;
+
+		// Input
+		System.out.println("Square root - using integers.");
+		System.out.println("===============================================");
+		System.out.println("Radicand: ");
+		radicand = scanner.nextInt();
+
+		// Process
+		root = sqtRoot.squareRoot(radicand);
+
+		// Output
+		System.out.println("The root of " + radicand + " is " + root + ".");
 
 	}
 
